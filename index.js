7fa0dd21807ab97454813a8a3d0e83e1c9f21dd0
MattMgr <matteo.mgr6@gmail.com>
2024-04-19 14:44:37 +0200

    styling + js

---
+++ index.js
@@ -0,0 +1,51 @@
+const toggleView = document.getElementById("toggle-view");
+const table = document.querySelector(".table-container");
+const grid = document.querySelector(".grid-container");
+
+toggleView.addEventListener("change", function () {
+  if (table.classList.contains("show") && grid.classList.contains("hide")) {
+    grid.classList.replace("hide", "show");
+    table.classList.replace("show", "hide");
+    sessionStorage.setItem("view", "grid");
+    toggleView.checked = true;
+  } else if (
+    table.classList.contains("hide") &&
+    grid.classList.contains("show")
+  ) {
+    table.classList.replace("hide", "show");
+    grid.classList.replace("show", "hide");
+    sessionStorage.setItem("view", "table");
+    toggleView.checked = false;
+  }
+});
+
+if (
+  sessionStorage.getItem("view") == "table" &&
+  !table.classList.contains("show") &&
+  !grid.classList.contains("hide") &&
+  !table.classList.contains("hide") &&
+  !grid.classList.contains("show")
+) {
+  table.classList.add("show");
+  grid.classList.add("hide");
+  toggleView.checked = false;
+} else if (
+  sessionStorage.getItem("view") == "grid" &&
+  !table.classList.contains("show") &&
+  !grid.classList.contains("hide") &&
+  !table.classList.contains("hide") &&
+  !grid.classList.contains("show")
+) {
+  table.classList.add("hide");
+  grid.classList.add("show");
+  toggleView.checked = true;
+} else if (sessionStorage.getItem("view") == "table") {
+  table.classList.replace("hide", "show");
+  grid.classList.replace("show", "hide");
+} else if (sessionStorage.getItem("view") == "grid") {
+  grid.classList.replace("hide", "show");
+  table.classList.replace("show", "hide");
+} else {
+  table.classList.add("show");
+  grid.classList.add("hide");
+}
