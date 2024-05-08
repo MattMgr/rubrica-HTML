95ebfbcec23285c203ed80917bf3dfd828fbfc08
MattMgr <matteo.mgr6@gmail.com>
2024-05-08 09:58:05 +0200

    forms.

--- index.js
+++ index.js
@@ -1,59 +1,3 @@
-// TOGGLE VIEW
-
-const toggleView = document.getElementById("toggle-view");
-const table = document.querySelector(".table-container");
-const grid = document.querySelector(".grid-container");
-const tableIcon = document.querySelector(".table");
-const gridIcon = document.querySelector(".grid");
-
-toggleView.addEventListener("change", function () {
-  if (grid.classList.contains("hide") && table.classList.contains("show")) {
-    grid.classList.replace("hide", "show");
-    table.classList.replace("show", "hide");
-    sessionStorage.setItem("view", "grid");
-    toggleView.checked = true;
-  } else if (
-    table.classList.contains("hide") &&
-    grid.classList.contains("show")
-  ) {
-    table.classList.replace("hide", "show");
-    grid.classList.replace("show", "hide");
-    sessionStorage.setItem("view", "table");
-    toggleView.checked = false;
-  }
-});
-
-if (
-  sessionStorage.getItem("view") == "table" &&
-  !table.classList.contains("show") &&
-  !grid.classList.contains("hide") &&
-  !table.classList.contains("hide") &&
-  !grid.classList.contains("show")
-) {
-  table.classList.add("show");
-  grid.classList.add("hide");
-  toggleView.checked = false;
-} else if (
-  sessionStorage.getItem("view") == "grid" &&
-  !table.classList.contains("show") &&
-  !grid.classList.contains("hide") &&
-  !table.classList.contains("hide") &&
-  !grid.classList.contains("show")
-) {
-  table.classList.add("hide");
-  grid.classList.add("show");
-  toggleView.checked = true;
-} else if (sessionStorage.getItem("view") == "table") {
-  table.classList.replace("hide", "show");
-  grid.classList.replace("show", "hide");
-} else if (sessionStorage.getItem("view") == "grid") {
-  grid.classList.replace("hide", "show");
-  table.classList.replace("show", "hide");
-} else {
-  table.classList.add("show");
-  grid.classList.add("hide");
-}
-
 // GET DATA FROM JSONS-ERVER
 
 const tableElement = document.querySelector(".table-container");
@@ -136,9 +80,66 @@ postForm.addEventListener("submit", (event) => {
 // deleteIcon.addEventListener("click", deleteContact)
 
 tableElement.addEventListener("click", (event) => {
+  event.stopPropagation();
   if (event.target.id === "delete-icon") {
     const contactId = divId.getAttribute("class");
     console.log(contactId);
     sendHttpRequest("DELETE", `http://localhost:3000/contacts/${contactId}`);
   }
 });
+
+// TOGGLE VIEW
+
+const toggleView = document.getElementById("toggle-view");
+const table = document.querySelector(".table-container");
+const grid = document.querySelector(".grid-container");
+const tableIcon = document.querySelector(".table");
+const gridIcon = document.querySelector(".grid");
+
+toggleView.addEventListener("change", function () {
+  if (grid.classList.contains("hide") && table.classList.contains("show")) {
+    grid.classList.replace("hide", "show");
+    table.classList.replace("show", "hide");
+    sessionStorage.setItem("view", "grid");
+    toggleView.checked = true;
+  } else if (
+    table.classList.contains("hide") &&
+    grid.classList.contains("show")
+  ) {
+    table.classList.replace("hide", "show");
+    grid.classList.replace("show", "hide");
+    sessionStorage.setItem("view", "table");
+    toggleView.checked = false;
+  }
+});
+
+if (
+  sessionStorage.getItem("view") == "table" &&
+  !table.classList.contains("show") &&
+  !grid.classList.contains("hide") &&
+  !table.classList.contains("hide") &&
+  !grid.classList.contains("show")
+) {
+  table.classList.add("show");
+  grid.classList.add("hide");
+  toggleView.checked = false;
+} else if (
+  sessionStorage.getItem("view") == "grid" &&
+  !table.classList.contains("show") &&
+  !grid.classList.contains("hide") &&
+  !table.classList.contains("hide") &&
+  !grid.classList.contains("show")
+) {
+  table.classList.add("hide");
+  grid.classList.add("show");
+  toggleView.checked = true;
+} else if (sessionStorage.getItem("view") == "table") {
+  table.classList.replace("hide", "show");
+  grid.classList.replace("show", "hide");
+} else if (sessionStorage.getItem("view") == "grid") {
+  grid.classList.replace("hide", "show");
+  table.classList.replace("show", "hide");
+} else {
+  table.classList.add("show");
+  grid.classList.add("hide");
+}
